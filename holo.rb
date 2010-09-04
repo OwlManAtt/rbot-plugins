14d6be7c33399499f0bb117c0fdb4fcf135ec9c3
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2010-09-04 15:27:39 +0000

    Now uses the shipment tracker stuff.

--- holo.rb
+++ holo.rb
@@ -6,9 +6,10 @@ class HoloPlugin < Plugin
   def initialize
     @tracking_numbers = {
       #'Case' => '1ZX799390310081385',
-      'Disks' => '1Z462E560321092067',
-      'Fans' => '1ZX799470329512449',
       #'Everything else' => '1ZX799331231131463'
+      'Disks' => {:number => '1Z462E560321092067', :courier => 'ups'},
+      'Fans' => {:number => '1ZX799470329512449', :courier => 'ups'},
+      'Monitor' => {:number => '134619891746049', :courier => 'fedex'},
     }
     super
   end # initialize 
@@ -18,17 +19,11 @@ class HoloPlugin < Plugin
   end # status
 
   def status_fetch
-    result = @tracking_numbers.map do |label,number|
-      doc = Nokogiri::HTML(open("http://wwwapps.ups.com/WebTracking/processInputRequest?sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_US&InquiryNumber1=#{number}&track.x=0&track.y=0"))
-      latest_row = doc.search('fieldset[@id=showPackageProgress]//tr')[1]
-      if latest_row
-        datum = {
-          :location => latest_row.children[0].children.first.content.ircify_html,
-          :time => latest_row.children[2].content.ircify_html << " " << latest_row.children[4].content.ircify_html,
-          :activity => latest_row.children[6].content.ircify_html,
-        }
-        
-        "\00303#{label}\003: #{datum[:activity]} @ #{datum[:time]} - #{datum[:location]}"
+    result = @tracking_numbers.map do |label,info|
+      status = ShipmentScreenScraper.send "fetch_#{info[:courier]}", info[:number]     
+
+      if status
+        "\00303#{label}\003: #{status.ircify}"
       else
         "\00303#{label}\003: No information available."
       end
