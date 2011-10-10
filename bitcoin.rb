7240f3764b90f06aca7e6071c67852cf90988853
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2011-10-10 16:09:50 +0000

    dont even know what this shit does

--- bitcoin.rb
+++ bitcoin.rb
@@ -9,13 +9,18 @@ class BitcoinPlugin < Plugin
     def status(m, params)
       json = open("http://mining.bitcoin.cz/accounts/profile/json/5339-b08a146b04934cec6e6d8041b014ec2f").read
       profile = JSON.parse(json)
-  
-      values = [profile['unconfirmed_reward'].to_f, profile['confirmed_reward'].to_f]
-      values << values.sum
+      coins = profile['unconfirmed_reward'].to_f + profile['confirmed_reward'].to_f  
+      m.reply "Balance: #{coins} BTC"
+
+      #ticker_json = open('https://mtgox.com/code/data/ticker.php', {:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE}).read
+      #t = JSON.parse(ticker_json)
+      #t = t['ticker']
+      #coin_value = coins * t['buy'].to_f
+     
+      #values = [coins, coin_value]
+      #display = values.map {|f| sprintf('%.2f',f) }
       
-      display = values.map {|f| sprintf('%.2f',f) }
-      
-      m.reply "#{display[0]} unconfirmed + #{display[1]} confirmed = \00309#{display[2]} BTC\003"
+      #m.reply "#{display[0]} BTC = #{display[1]} USD"
     end # summon
 end # BitcoinPlugin
 plugin = BitcoinPlugin.new
